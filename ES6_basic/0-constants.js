9735e0f18757989ee50fd2898c2089d7d28a18bf
zahraiva <zaraiv06@gmail.com>
2024-11-14 11:10:07 +0400

    done

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,14 +1,15 @@
 export function taskFirst() {
-    const task1 = 'I prefer const when I can.';
+    const task1 = "I prefer const when I can.";
     return task1;
   }
   
   export function getLast() {
-    return ' is okay';
+    return " is okay";
   }
   
   export function taskNext() {
-    let combination = 'But sometimes let';
+    let combination = "But sometimes let";
     combination += getLast();
+  
     return combination;
   }
