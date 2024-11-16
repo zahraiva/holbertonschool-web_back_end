9318d30afb12b73c81b75c4444a081f0649e6d4e
zahraiva <zaraiv06@gmail.com>
2024-11-17 03:20:06 +0400

    done

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export default function createReportObject(employeesList) {
+    return {
+        allEmployees: { ...employeesList },
+        getNumberOfDepartments() {
+          return Object.keys(this.allEmployees).length;
+        }
+      };
+}
