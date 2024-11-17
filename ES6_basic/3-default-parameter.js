1f619aa4eaf068a06906f0f72dd6f0beb684376a
zahraiva <zaraiv06@gmail.com>
2024-11-17 17:02:31 +0400

    wcdwcdscds

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,3 +1,9 @@
+// getSumOfHoods.js
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
-    return initialNumber + expansion1989 + expansion2019;
+  // Ensure initialNumber is a valid number
+  if (isNaN(initialNumber)) {
+      throw new Error("initialNumber must be a valid number");
+  }
+
+  return initialNumber + expansion1989 + expansion2019;
 }
