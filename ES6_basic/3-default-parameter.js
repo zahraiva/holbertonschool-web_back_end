831a5fe91f0e757e0b3dd3cbcda0525336cd24ba
zahraiva <zaraiv06@gmail.com>
2024-11-17 17:03:24 +0400

    rvfvd

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,7 +1,5 @@
-// getSumOfHoods.js
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
-  // Ensure initialNumber is a valid number
-  if (isNaN(initialNumber)) {
+if (isNaN(initialNumber)) {
       throw new Error("initialNumber must be a valid number");
   }
 
